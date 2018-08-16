83819edda54a21a7168e618a3875ca6e53697768
j13tw <j13tw@yahoo.com.tw>
2018-08-16 15:20:15 +0800

    add NFC control class

--- client/app.py
+++ client/app.py
@@ -1,61 +1,39 @@
-import serial, time
+import os, sys
 from serial import SerialException
+from flask import Flask, request, render_template, redirect, url_for
+from NFC_Class import NFC
 
-# wake up reader
-# send: 55 55 00 00 00 00 00 00 00 00 00 00 00 00 00 00 ff 03 fd d4 14 01 17 00
-# return: 00 00 FF 00 FF 00 00 00 FF 02 FE D5 15 16 00
- 
-# get firmware
-# send: 00 00 FF 02 FE D4 02 2A 00
-# return: 00 00 FF 00 FF 00 00 00 FF 06 FA D5 03 32 01 06 07 E8 00
- 
-# read the tag
-# send: 00 00 FF 04 FC D4 4A 01 00 E1 00
-# return: 00 00 FF 00 FF 00 00 00 FF 0C F4 D5 4B 01 01 00 04 08 04 XX XX XX XX 5A 00
-# XX is tag.
 
-class NFC(NFC_locate):
-    def __init__(NFC_locate):
-        ser = serial.Serial(NFC_locate, 115200, timeout=1)
-        wakeReader = bytearray([85, 85, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 255, 3, 253, 212, 20, 1, 23, 0])
-        deviceInfo = bytearray([0, 0, 255, 2, 254, 212, 2, 42, 0])
-        readTag = bytearray([0, 0, 255, 4, 252, 212, 74, 1, 0, 225, 0])
+NFC_locate = "COM6"
+STM32_locate = "COM7"
+LORA_locate = "COM8"
 
-    def wake():
-        global wakeReader
-        ser.write(wakeReader)
-        Ack = str(ser.read(15)).upper().split('\\X')
-        response = "return wake block = "
-        for x in range(1, len(Ack)):
-            if x == len(Ack)-1:
-                response = response + ' ' + Ack[x].split("'")[0]
-            else:    
-                response = response + ' ' + Ack[x]
-        if response != "": response = "OK"
-        else response = "ERROR"
-        return response
+# fake data 
 
-    def info():
-        global deviceInfo
-        ser.write(deviceInfo)
-        Ack = str(ser.read(19)).upper().split('\\X')
-        response = "return info block = "
-        for x in range(1, len(Ack)):
-            if x == len(Ack)-1:
-                response = response + ' ' + Ack[x].split("'")[0]
-            else:    
-                response = response + ' ' + Ack[x]
-        if response != "": response = "OK"
-        else response = "ERROR"
-        return response
+def STM32_response(STM32_locate):
+    return = "OK"
 
-    def read():
-        global readTag
-        ser.write(readTag)
-        Ack = str(ser.read(45)).upper().split('\\X')
-        response = "return card block = "
-        for x in range(10, 20):
-            response = response + Ack[x]
-        print(response)
-        if response != "": response = "OK"
-        else response = "ERROR"
+# LORA check "mod get_ver"
+def LORA_response(STM32_locate):
+    return = "OK"
+
+# system boot up
+def Client_hotware_boot():
+    # STM32_check_device
+
+    # RFID_check_device
+    while (STM32.STM32_response(STM32_locate) != "RFID_CHECK"):
+        if (NFC.wake(NFC.locate) == "OK"):
+            STM32_write("RFID_OK")
+        else:
+            STM32_write("RFID_ERROR")
+    # LORA_check_device
+    while (STM32.STM32_response(STM32_locate) != "LORA_CHECK")
+        if (LORA.LORA_response(LORA_locate(LORA_locate)) == "OK")
+            STM32_write("LORA_OK")
+        else:
+            STM32_write("LORA_ERROR")
+    
+
+def Client_software_boot():
+
